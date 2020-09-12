4be1d41d15f3df6f033e9c507203d185d63b11a4
Bishwajit Jha <ajitjha393@gmail.com>
2020-09-12 15:41:59 +0530

    Added Input Box and Button for Goal

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { View, TextInput, Button } from 'react-native'
 
 export default function App() {
 	return (
-		<View style={styles.container}>
+		<View style={{ padding: 50 }}>
+			<View>
+				<TextInput
+					placeholder="Write your Goal!"
+					style={{
+						borderBottomColor: 'black',
+						borderBottomWidth: 1,
+						padding: 10,
+					}}
+				/>
+				<Button title="Add" />
+			</View>
+			<View></View>
 			<StatusBar style="auto" />
 		</View>
 	)
 }
-
-const styles = StyleSheet.create({})
